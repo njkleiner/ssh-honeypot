2246e17480e050012f67bc43ac78def418a057d5
Noah Kleiner <noah@njkleiner.com>
2024-07-08 18:03:45 +0200

    sandbox: add unit test for `Driver.Close` method
    This change implements a white box test for the `Driver.Close` method,
    which ensures that the specified behavior is implemented correctly.
    
    Note that we will evolve (update) this unit test when we continue to
    refactor the driver cleanup behavior and worker loop implementation.

--- internal/sandbox/driver_test.go
+++ internal/sandbox/driver_test.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 	"os"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/njkleiner/ssh-honeypot/internal/config"
@@ -84,5 +85,52 @@ func TestSession(t *testing.T) {
 
 	if err := dv.Destroy(ref); err != nil {
 		t.Errorf("cannot destroy container (ref=%s): %v", ref, err)
+	}
+}
+
+func TestDriverClose(t *testing.T) {
+	if v := os.Getenv("CI_SKIP_TEST"); strings.Contains(v, "docker") {
+		t.Skipf("skipping test: CI_SKIP_TEST=%q", v)
+	}
+
+	// The [Driver] does internal logging, so make sure the default logger is set.
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelDebug, // makes sense during testing
+	})))
+
+	var cfg config.File
+
+	cfg.SetDefaults()
+
+	cfg.Sandbox.ReadyQueueSize = 3 // hard-code in case the default changes
+
+	dv, err := NewDriver(cfg)
+
+	if err != nil {
+		t.Fatalf("cannot create sandbox driver: %v", err)
+	}
+
+	closeDriver := sync.OnceFunc(dv.Close) // see comment below
+
+	// NOTE: we want to make sure that we still perform a graceful
+	// shutdown and guest cleanup if the test exits (fails) early.
+	// IMPORTANT: since we now signal the worker loop to return by
+	// closing a channel, we must never call [Driver.Close] twice.
+	t.Cleanup(closeDriver)
+
+	_, err = dv.Acquire(context.Background())
+
+	if err != nil {
+		t.Fatalf("cannot acquire container: %v", err)
+	}
+
+	closeDriver() // blocks until cleanup is done
+
+	if got, want := len(dv.alive), 0; got != want {
+		t.Errorf("invalid number of containers alive: got=%d; want=%d", got, want)
+	}
+
+	if got, ok := <-dv.ready; ok && got != "" {
+		t.Errorf("ready queue must be empty (got=%q)", got)
 	}
 }
